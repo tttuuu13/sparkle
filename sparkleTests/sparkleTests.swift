2418abdc40bf2c200b0487ce7da0fc35cbb24f16
tttuuu13 <tim.ib.222005@gmail.com>
2024-09-26 13:13:54 +0300

    Initial Commit

---
+++ sparkleTests/sparkleTests.swift
@@ -0,0 +1,17 @@
+//
+//  sparkleTests.swift
+//  sparkleTests
+//
+//  Created by тимур on 26.09.2024.
+//
+
+import Testing
+@testable import sparkle
+
+struct sparkleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
